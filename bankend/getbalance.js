f3f603ac343315ea349d5c8720163589f7b689be
Yuval Kashtan <yuvalkashtan@gmail.com>
2018-09-13 13:45:25 +0300

    wIP - actual get balance

--- bankend/getbalance.js
+++ bankend/getbalance.js
@@ -1,6 +1,35 @@
 var neo4j = require('neo4j-driver').v1;
 
+const neo4jUser = process.env.NEO4J_USER;
+const neo4jPassword = process.env.NEO4J_PASSWORD;
+const neo4jEndpoint = process.env.NEO4J_ENDPOINT;
+
+function getNeo4jDriver()
+{
+        console.log("Connecting to neo4j");
+        var driver = neo4j.driver(neo4jEndpoint, neo4j.auth.basic(neo4jUser, neo4jPassword));
+        console.log("Created neo4j driver.");
+        return driver;
+}
+
 module.exports.get_balance_for_user = async (username) => {
-	return 100;
+    var driver = getNeo4jDriver();
+    const session = driver.session();
+    const result = await session.run("Match (n:User) WHERE n.name='"+username+"' RETURN n");
+    session.close();
+    driver.close();
+
+    if (result.records.length == 0) {
+        return null;
+    }
+
+    record = result.records[0];
+    // get value and transform from neo4j-style-numbers
+    var curBalance = record._fields[0].properties.balance;
+    if ('low' in curBalance) { // if Neo4j long object, take only number.
+        curBalance = curBalance.low;
+    }
+    console.log("getBalance result:" + curBalance);
+    return Number(curBalance);
 }
 
