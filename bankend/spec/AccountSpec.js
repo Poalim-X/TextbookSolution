3d8f351557a79badc8214a64633e18bdd41725d8
Shaul Daon <daonsh@gmail.com>
2018-11-04 14:18:03 +0200

    ensure user exists for tests
    also fix positive balance test so it will check for the default balance (and not 100)

--- bankend/spec/AccountSpec.js
+++ bankend/spec/AccountSpec.js
@@ -1,14 +1,18 @@
 const Account = require('../account');
 
 describe("Account", function() {
+    beforeAll(async function() {
+        await Account.ensure_account_exists("TEST2-at-kashyoo.com");
+    });
+
     it("return null when username does not exist", async function() {
         var balance = await Account.get_balance_for_user("nonexist");
 	expect (balance).toEqual(null);
     });
 
-    it("return 100 for test user", async function() {
+    it("return 7000 for test user", async function() {
         var balance = await Account.get_balance_for_user("TEST2-at-kashyoo.com");
-	expect (balance).toEqual(100);
+	expect (balance).toEqual(7000);
     });
 });
 
