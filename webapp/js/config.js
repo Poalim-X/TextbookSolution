2eac2cea1f1465d37af2943be4be0665ac67d61a
julie bareket <badulina@gmail.com>
2018-11-05 12:28:23 +0200

    update pool to us-east-1

--- webapp/js/config.js
+++ webapp/js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_V9mc5CD4s',
-        userPoolClientId: '2n4ispca2b4gisfgh4t73qpq2f',
+        userPoolId: 'us-east-1_X47k1kexW',
+        userPoolClientId: '2ovn6br744neoj39u16pcvd6fa',
         region: 'us-east-1'
     },
     api: {
